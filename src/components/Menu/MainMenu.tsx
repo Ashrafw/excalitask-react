c14ca82ed5dc436e606183537557c302e643495c
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2023-12-27 10:02:52 +0000

    scale fix

--- src/components/Menu/MainMenu.tsx
+++ src/components/Menu/MainMenu.tsx
@@ -13,14 +13,16 @@ const MainMenu = () => {
     <div className="relative">
       <button
         onClick={() => setIsOpen((prev) => !prev)}
-        className={` p-2 flex shadow-sm justify-center items-center rounded bg-white text-gray-900  hover:bg-slate-100 text-2xl`}
+        className={` p-2 flex shadow-sm justify-center items-center rounded bg-white text-gray-700  hover:bg-slate-100 text-2xl`}
       >
         <IoInvertMode />
       </button>
       {isOpen ? (
         <div className=" absolute bg-white/85 mt-2 p-2 rounded-md  flex flex-col gap-4 right-0 z-20 shadow-md">
           <div>
-            <h2 className=" text-sm pb-2 underline font-bold">Background</h2>
+            <h2 className=" text-sm pb-2 underline font-bold text-gray-800">
+              Background
+            </h2>
             <div className=" grid grid-cols-3  gap-x-4 gap-y-2 items-center">
               <button
                 onClick={() => setBackgroundColour("bg-slate-100")}
@@ -65,20 +67,11 @@ const MainMenu = () => {
             </div> */}
           </div>
           <div>
-            <h2 className=" text-sm pb-2 underline font-bold">Size</h2>
+            <h2 className=" text-sm pb-2 underline font-bold text-gray-800">Size</h2>
             <div className=" flex  gap-2 items-center">
               <button
-                onClick={() => setZoomNum("zoom90")}
-                className={` w-12 h-10 bg-slate-50 rounded shadow-md  ${
-                  zoomNum === "zoom90" ? " ring-[3px] ring-blue-400 " : ""
-                } `}
-              >
-                {" "}
-                XS
-              </button>{" "}
-              <button
                 onClick={() => setZoomNum("zoom100")}
-                className={` w-12 h-10 bg-slate-50 rounded shadow-md  ${
+                className={` w-12 h-10 bg-white rounded shadow-md  ${
                   zoomNum === "zoom100" ? " ring-[3px] ring-blue-400 " : ""
                 } `}
               >
@@ -87,7 +80,7 @@ const MainMenu = () => {
               </button>
               <button
                 onClick={() => setZoomNum("zoom125")}
-                className={` w-12 h-10 bg-slate-50rounded shadow-md ${
+                className={` w-12 h-10 bg-white rounded shadow-md ${
                   zoomNum === "zoom125" ? " ring-[3px] ring-blue-400" : ""
                 }`}
               >
@@ -96,7 +89,7 @@ const MainMenu = () => {
               </button>
               <button
                 onClick={() => setZoomNum("zoom150")}
-                className={` w-12 h-10  bg-slate-50 rounded shadow-md ${
+                className={` w-12 h-10 bg-white rounded shadow-md ${
                   zoomNum === "zoom150" ? " ring-[3px] ring-blue-400" : ""
                 }`}
               >
@@ -105,7 +98,7 @@ const MainMenu = () => {
               </button>
               <button
                 onClick={() => setZoomNum("zoom160")}
-                className={` w-12 h-10  bg-slate-50 rounded shadow-md ${
+                className={` w-12 h-10 bg-white rounded shadow-md ${
                   zoomNum === "zoom160" ? " ring-[3px] ring-blue-400" : ""
                 }`}
               >
